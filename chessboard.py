755b8ee6381976a60a8e87694cc2756faf1216db
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-29 23:30:52 +0100

    Update chessboard.py

--- chessboard.py
+++ chessboard.py
@@ -78,22 +78,58 @@ def echiquierVide():
     return [[None] * 8] * 8
 
 def echiquierInitialise():
-    return rules.listeDesPieces('N')[::-1] + [[None] * 8] * 4 + rules.listeDesPieces('B')
+    return rules.listeDesPieces('N')[::-1] + [[' 1'] * 8] * 4 + rules.listeDesPieces('B')
 
 
 
+
+#==============================================================================
+# Déplacement
+#==============================================================================
+
+def deplacerPiece(echiquier, caseDepart, caseArrive):
+    
+    ec = echiquier
+    
+    ligDep, colDep = nomCaseToIndex(caseDepart)
+    ligArr, colArr = nomCaseToIndex(caseArrive)
+    
+    print(str(ligDep) + "-" + str(colDep))
+    print(str(ligArr) + "-" + str(colArr))
+
+    valeur = echiquier[ligDep][colDep]
+     
+    ec[ligDep][colDep] = None  
+    ec[ligArr][colArr] = valeur
+    
+#    print(echiquier)
+    
+    return echiquier
 
 #==============================================================================
 # Vérification déplacement dans la zone
 #==============================================================================
 
+def deplacementsPossibles(echiquier, piece):
+    pass
+
+def afficherDeplacementsPossibles(echiquier, piece):
+    pass
+
+#==============================================================================
+# Autres fonctions
+#==============================================================================
+
+def nomCaseToIndex(nomCase):
+    return int(nomCase[1]) - 1, ['A','B','C','D','E','F','G','H'].index(nomCase[0])
 
 #print("‎• or <>")
 
 
 
-
-
+ec = echiquierInitialise()
+afficherEchiquier(ec)
+afficherEchiquier(deplacerPiece(ec, "A7", "A5"))
 
 
 
