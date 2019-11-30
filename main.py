a18b80890e1c86d1bab5bf0feecea86a5361a627
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-30 13:22:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import chessboard
+from chessboard import Echiquier
 import player
 import ia
 
 
 
+
 echiquier = Echiquier()
+echiquier.afficher()
 
-while not echiquier.isEchecEtMat():
-    
-    #au blanc de jouer
-    
-    #à l'IA de jouer
-
-    pass
+#while not echiquier.isEchecEtMat():
+#    
+#    #au blanc de jouer
+#    
+#    #à l'IA de jouer
+#
+#    pass
 
 
