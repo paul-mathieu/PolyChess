d672f6b51b6c277faa92c04bf59226f4c4360ced
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-11-30 21:46:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,34 @@ import ia
 echiquier = Echiquier()
 echiquier.afficher()
 
+#print(echiquier.isEchecEtMat())
+#print(echiquier.couleurEchecEtMat())
+
 #while not echiquier.isEchecEtMat():
 #    
-#    #au blanc de jouer
+#    #au joueur blanc de déplacer une pièce
 #    
 #    #à l'IA de jouer
 #
 #    pass
 
+#une fois la boucle finie
+
+
+
+
+
+
+#
+#Fait jusqu'à présent :
+#    - classe Echiquier et classe Piece
+#    - définition des pieces
+#    - initialisation de l'échiquier
+#    - gestion des déplacements dans l'échiquier
+#    - vérification de l'échec et mat
+#    
+#À faire bientôt :
+#    - déplacements possibles
+#    - affichage des déplacements possibles
+#
 
