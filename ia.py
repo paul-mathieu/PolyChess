f3b9748a31fc51b314eec64f83fca8ef22c0ac99
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-12-02 14:04:26 +0100

    Update ia.py

--- ia.py
+++ ia.py
@@ -1,7 +1,51 @@
 
 
-import rules
-import chessboard
+class MeilleurMouvement:
+    
+    def __init__(self, echiquier, couleur):
+        
+        self.echiquier = echiquier
+        
+        self.couleur = couleur
+        self.couleurOpposee = 'noir' if couleur == 'blanc' else 'noir'
+        
+        
+        self.valeurPions = {
+                ('Pion', self.couleur) : -10,
+                ('Tour', self.couleur) : -50,
+                ('Cavalier', self.couleur) : -30,
+                ('Fou', self.couleur) : -30,
+                ('Dame', self.couleur) : -90,
+                ('Roi', self.couleur) : -900,
+                ('Pion', self.couleurOpposee) : 10,
+                ('Tour', self.couleurOpposee) : 50,
+                ('Cavalier', self.couleurOpposee) : 30,
+                ('Fou', self.couleurOpposee) : 30,
+                ('Dame', self.couleurOpposee) : 90,
+                ('Roi', self.couleurOpposee) : 900
+                }
+    
+    
+    def mouvementsInterressantsCeTour(self):
+        
+        liste = 
+        return self.echiquier.
+    
+    def mouvement(self, indexDeplacement, valerDeplacement):
+        
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
