344a9709b91cdaf50674e30f7c1204dc75ea238f
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-12-08 19:52:57 +0100

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  8 19:28:06 2019
+
+@author: Anyone
+"""
+
+
+
+
+
+
+
+
+
+
+print("/!\\ Empire is coming /!\\".center(10 * 4," ") + "\n" + "\\_/ " * 10 + "\n" + "/¯\\ " * 10, end = "\n\n\n\n\n")
+
+
+
+
+
+
+phoenix = [
+"      _,     ,_",
+"    .'/  ,_   \\'.",
+"   |  \\__( >__/  |",
+"   \\             /",
+"    '-..__ __..-'",
+"         /_\\"]
+
+print("\n".join(phoenix))
+
