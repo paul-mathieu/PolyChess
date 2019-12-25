81fd05b310c6cd9db349c28d9f9868f89319d334
Paul Mathieu <56612315+paul-mathieu@users.noreply.github.com>
2019-12-25 23:59:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from chessboard import Echiquier
 from ia import IA, Mouvement
 
 
-
 # =============================================================================
 # Gestion de l'interface - boucle du moteur
 # =============================================================================
@@ -57,46 +56,46 @@ str_ne_plus_afficher_laide = "Pour ne plus afficher l'aide, entrez : fin aide\n\
 str_afficher_laide = "Pour afficher l'aide, entrez : aide\n\n"
 ne_plus_afficher_laide = False
 
-for numero_du_tour in range(50):
-    
-    #-- le joueur joue --
-     
-    #qu'est-ce qu'il veut faire ?
-    while True:
-        
-        if ne_plus_afficher_laide:
-            entree_joueur = input(str_afficher_laide)
-        else:
-            entree_joueur = input(aide_joueur + str_ne_plus_afficher_laide)
-        
-        #ne plus afficher l'aide
-        if entree_joueur == 'fin aide':
-            ne_plus_afficher_laide = True
-        elif entree_
-        
-        # 1 - si il veut jouer
-        if entree_joueur[:-6] in [element[:-43] for element in liste_aide_joueur[1][1]] + ['1']:
-            break
-        
-        # 2 - "Pour connaitre les coups possibles"
-        elif entree_joueur in liste_aide_joueur[2][1] + ['2']:
-            pass
-        
-        # 3 - "Pour connaitre la liste des coups possibles d'une piece"
-        elif entree_joueur in liste_aide_joueur[3][1] + ['3']:
-            pass
-        
-        # 4 - "Pour afficher l'échiquier"
-        elif entree_joueur in liste_aide_joueur[4][1] + ['4']:
-            pass
-        
-        # 5 - "Pour afficher l'échiquier avec les coups possibles d'une piece"
-        elif entree_joueur in liste_aide_joueur[5][1] + ['5']:
-            pass
-    
-    break
-   
-    
+#for numero_du_tour in range(50):
+#    
+#    #-- le joueur joue --
+#     
+#    #qu'est-ce qu'il veut faire ?
+#    while True:
+#        
+#        if ne_plus_afficher_laide:
+#            entree_joueur = input(str_afficher_laide)
+#        else:
+#            entree_joueur = input(aide_joueur + str_ne_plus_afficher_laide)
+#        
+#        #ne plus afficher l'aide
+#        if entree_joueur == 'fin aide':
+#            ne_plus_afficher_laide = True
+##        elif entree_
+#        
+#        # 1 - si il veut jouer
+#        if entree_joueur[:-6] in [element[:-43] for element in liste_aide_joueur[1][1]] + ['1']:
+#            break
+#        
+#        # 2 - "Pour connaitre les coups possibles"
+#        elif entree_joueur in liste_aide_joueur[2][1] + ['2']:
+#            pass
+#        
+#        # 3 - "Pour connaitre la liste des coups possibles d'une piece"
+#        elif entree_joueur in liste_aide_joueur[3][1] + ['3']:
+#            pass
+#        
+#        # 4 - "Pour afficher l'échiquier"
+#        elif entree_joueur in liste_aide_joueur[4][1] + ['4']:
+#            pass
+#        
+#        # 5 - "Pour afficher l'échiquier avec les coups possibles d'une piece"
+#        elif entree_joueur in liste_aide_joueur[5][1] + ['5']:
+#            pass
+#    
+#    break
+#   
+#    
     
     #verification de fin de partie
     
@@ -111,28 +110,28 @@ for numero_du_tour in range(50):
 # Appel de l'échiquier
 #==============================================================================
 
-#
-#echiquier = Echiquier()
-#echiquier.afficher()
-#
-#print(echiquier.listeCoupsPossiblesFormatCase('C7'))
-#print(echiquier.listeCoupsPossiblesFormatCase('B7'))
-#
-##echiquier.afficherCoupsPossibles('D7')
-#
-#echiquier.deplacerPiece('A2', 'A3')
-#echiquier.afficher()
-#
-#echiquier.deplacerPiece('C2', 'E4')
-#echiquier.afficher() 
-#
-#echiquier.afficherCoupsPossibles('E4')
-#
-##print(echiquier.listeDeplacementsPossibles('A6'))
-#
-##print(echiquier.isEchecEtMat())
-##print(echiquier.couleurEchecEtMat())
-#
+
+echiquier = Echiquier()
+echiquier.afficher()
+
+print(echiquier.listeCoupsPossiblesFormatCase('C7'))
+print(echiquier.listeCoupsPossiblesFormatCase('B7'))
+
+#echiquier.afficherCoupsPossibles('D7')
+
+echiquier.deplacerPiece('A2', 'A3')
+echiquier.afficher()
+
+echiquier.deplacerPiece('C2', 'E4')
+echiquier.afficher() 
+
+echiquier.afficherCoupsPossibles('E4')
+
+#print(echiquier.listeDeplacementsPossibles('A6'))
+
+#print(echiquier.isEchecEtMat())
+#print(echiquier.couleurEchecEtMat())
+
 
 
 
